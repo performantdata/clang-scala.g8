fcf5442e6d1a6e03bdc340a05264128e7d3d6ea8
Mike <performantdata@users.noreply.github.com>
2020-12-08 23:37:27 -0800

    add GitHub Actions workflow
    Also:
    - Switch from Googletest to Catch2.

--- src/main/g8/src/dependencies.cmake
+++ src/main/g8/src/dependencies.cmake
@@ -1,30 +1,30 @@
 # Make available to this project those packages on which we depend.
+# See https://cliutils.gitlab.io/modern-cmake/chapters/testing/googletest.html
 
 # `FetchContent` is the way to do this these days, not `ExternalProject`.
 # See https://crascit.com/2015/07/25/cmake-gtest/ .
 include(FetchContent)
 
-# Shim for pre-3.14 `FetchContent`.
-if(${CMAKE_VERSION} VERSION_LESS 3.14)
-  macro(FetchContent_MakeAvailable NAME)
-    FetchContent_GetProperties(${NAME})
-    if(NOT ${NAME}_POPULATED)
-      FetchContent_Populate(${NAME})
-      add_subdirectory(${${NAME}_SOURCE_DIR} ${${NAME}_BINARY_DIR})
-    endif()
-  endmacro()
-endif()
-
 FetchContent_Declare(
-  googletest
-  GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG        release-1.10.0
+  Catch2
+  GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+  GIT_TAG        v2.13.1
 )
+
+FetchContent_MakeAvailable(Catch2)
+list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/contrib)
+message(VERBOSE "Added ${catch2_SOURCE_DIR}/contrib to CMAKE_MODULE_PATH.")
+
+# FetchContent_Declare(
+#   googletest
+#   GIT_REPOSITORY https://github.com/google/googletest.git
+#   GIT_TAG        release-1.10.0
+# )
 
 # Downloading Google Test like this allows us to build it with the same compiler flags
 # as the rest of the code, potentially avoiding linkage issues.  (This is instead of
 # relying on the `GoogleTest` CMake module and having the compiled library already
 # installed.)
 # See https://github.com/google/googletest/tree/master/googletest/README.md#incorporating-into-an-existing-cmake-project .
-FetchContent_MakeAvailable(googletest)
+# FetchContent_MakeAvailable(googletest)
 
