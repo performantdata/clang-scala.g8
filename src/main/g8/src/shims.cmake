fdcda6f29346de29dcb63a5e3097bb8a9f705b71
Mike <performantdata@users.noreply.github.com>
2020-12-09 20:56:12 -0800

    try instantiating the template
    Also: fix the template $-escapes.

--- src/main/g8/src/shims.cmake
+++ src/main/g8/src/shims.cmake
@@ -1,13 +1,13 @@
 # Shim for pre-3.14 `FetchContent`.
 # See https://cliutils.gitlab.io/modern-cmake/chapters/projects/fetch.html
 # See https://cmake.org/cmake/help/latest/module/FetchContent.html#populating-the-content
-if(${CMAKE_VERSION} VERSION_LESS 3.14)
+if(\${CMAKE_VERSION} VERSION_LESS 3.14)
   macro(FetchContent_MakeAvailable NAME)
-    FetchContent_GetProperties(${NAME})
-    string(TOLOWER "${NAME}" lcName)
-    if(NOT ${lcName}_POPULATED)
-      FetchContent_Populate(${NAME})
-      add_subdirectory(${${lcName}_SOURCE_DIR} ${${lcName}_BINARY_DIR})
+    FetchContent_GetProperties(\${NAME})
+    string(TOLOWER "\${NAME}" lcName)
+    if(NOT \${lcName}_POPULATED)
+      FetchContent_Populate(\${NAME})
+      add_subdirectory(\${\${lcName}_SOURCE_DIR} \${\${lcName}_BINARY_DIR})
     endif()
   endmacro()
 endif()
